51c517700a6c7e14a1e8414642efc158439ee52d
sesemann <robsesemann@gmail.com>
2022-10-27 10:46:55 +0100

    Backend for removing wines from favourite wine list

--- routes/wine.js
+++ routes/wine.js
@@ -7,5 +7,6 @@ const wineCtrl = require('../controllers/wines');
 const isLoggedIn = require('../helper/isLoggedIn');
 
 router.post('/wine/add', wineCtrl.wine_create_post);
+router.delete("/wine/delete", wineCtrl.wine_delete_get)
 
 module.exports = router;
